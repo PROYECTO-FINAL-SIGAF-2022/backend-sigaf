d289a178eb47afe7c5680dee3739fb2d17a2aba8
danielRojas14 <danielrojas131415@gmail.com>
2022-09-01 16:45:37 -0300

    se esta desarrollando el controlador de unidades de medida

--- controllers/unidadesMedidas.controller.js
+++ controllers/unidadesMedidas.controller.js
@@ -11,3 +11,49 @@ export const getUnidadesMedidas = async (req, res) => {
     });
   }
 };
+
+export const getUnidadMedidaUnico = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const dato = await UnidadesMedidasModelo.findByPk(id);
+
+    res.json(dato);
+  } catch (error) {
+    return res.status(500).json({
+      message: error.message,
+    });
+  }
+};
+
+export const postUnidadMedida = async (req, res) => {
+  try {
+    const { descripcion_unidad_medida } = req.body;
+
+    const nuevaUnidadMedida = await UnidadesMedidasModelo.create({
+      descripcion_unidad_medida,
+    });
+
+    res.json({
+      msg: "La unidad de medida se creo correctamente",
+      nuevaUnidadMedida,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      message: error.message,
+    });
+  }
+};
+
+export const updateUnidadMedida = async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    const { descripcion_unidad_medida } = req.body[0];
+
+    const actualizarUnidadMedida = UnidadesMedidasModelo.findOne({
+      where: { id_unidad_medida: id },
+    });
+
+    actualizarUnidadMedida.descripcion_unidad_medida = actualizarUnidadMedida;
+  } catch (error) {}
+};
