bb808a38cbf42681de222f98b3acd51484dbc503
danielRojas14 <danielrojas131415@gmail.com>
2022-09-13 11:40:00 -0300

    se estan refactorizando los controladores, agregando los log

--- controllers/actividades.controllers.js
+++ controllers/actividades.controllers.js
@@ -1,11 +1,20 @@
 import { ActividadesModelo } from "../models/Actividades.model.js";
+import { LogSistema } from "../models/LogSistema.js";
 
 // Devuelve todos los actividadess de la colección
 export const getActividades = async (req, res) => {
   try {
-    const Datos = await ActividadesModelo.findAll(); // consulta para todos los documentos
+    const { id_usuario } = req.decoded.paramUsuario;
+
+    const actividad = await ActividadesModelo.findAll(); // consulta para todos los documentos
+
+    await LogSistema.create({
+      id_usuario,
+      descripcion_log: `El usuario con ID ${id_usuario} realizo la siguiente busqueda, `,
+    });
+
     // Respuesta del servidor
-    res.json(Datos);
+    res.json(actividad);
   } catch (error) {
     return res.status(500).json({
       message: error.message,
@@ -16,10 +25,20 @@ export const getActividades = async (req, res) => {
 export const getActividadUnico = async (req, res) => {
   try {
     const { id } = req.params;
-    const Datos = await ActividadesModelo.findByPk(id); // consulta para todos los documentos
+
+    const { id_usuario } = req.decoded.paramUsuario;
+
+    const actividad = await ActividadesModelo.findByPk(id); // consulta para todos los documentos
+
+    const dataLog = actividad[0].dataValues;
+
+    await LogSistema.create({
+      id_usuario,
+      descripcion_log: `El usuario con ID ${id_usuario} realizo la siguiente busqueda, ${JSON.stringify(dataLog)}`,
+    });
 
     // Respuesta del servidor
-    res.json(Datos);
+    res.json(actividad);
   } catch (error) {
     return res.status(500).json({
       message: error.message,
@@ -31,8 +50,15 @@ export const postActividad = async (req, res) => {
   try {
     const { descripcion_actividad } = req.body;
 
+    const { id_usuario } = req.decoded.paramUsuario;
+
     const nuevaActividad = await ActividadesModelo.create({
       descripcion_actividad,
+    });
+
+    await LogSistema.create({
+      id_usuario,
+      descripcion_log: `El usuario con ID ${id_usuario} realizo la siguiente creacion, ${nuevaActividad.dataValues}`,
     });
 
     res.status(201).json({
@@ -50,6 +76,7 @@ export const updateActividad = async (req, res) => {
   // console.log(req.body);
   try {
     const { id } = req.params;
+    const { id_usuario } = req.decoded.paramUsuario;
     // console.log(req.body);
     const { descripcion_actividad } = req.body;
     // console.log(id);
@@ -60,6 +87,11 @@ export const updateActividad = async (req, res) => {
     // console.log(descripcion_actividad);
     updateActiv.descripcion_actividad = descripcion_actividad;
     await updateActiv.save();
+
+    await LogSistema.create({
+      id_usuario,
+      descripcion_log: `El usuario con ID ${id_usuario} realizo la siguiente actualizacion, ${updateActiv.dataValues}`,
+    });
 
     res.status(200).json({
       msg: "La actividad se actualizo Correctamente",
@@ -75,6 +107,7 @@ export const updateActividad = async (req, res) => {
 export const deleteActividad = async (req, res) => {
   try {
     const { id } = req.params;
+    const { id_usuario } = req.decoded.paramUsuario;
 
     const delleteActiv = await ActividadesModelo.findOne({
       where: { id_actividad: id },
@@ -82,6 +115,11 @@ export const deleteActividad = async (req, res) => {
 
     delleteActiv.activo = false;
     await delleteActiv.save();
+
+    await LogSistema.create({
+      id_usuario,
+      descripcion_log: `El usuario con ID ${id_usuario} realizo la siguiente eliminacion, ${delleteActiv.dataValues}`,
+    });
 
     res.status(200).json({
       message: `Se elimino la actividad con el ID: ${id}`,
